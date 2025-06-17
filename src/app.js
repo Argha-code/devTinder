53b0229d02f18726603e71397f95310c0e045396
Argha-code <arghapanda19@gmail.com>
2025-06-17 11:03:15 +0530

    prac-multiple route handelers

--- src/app.js
+++ src/app.js
@@ -2,30 +2,33 @@ const express = require("express");  // require express from module
 
 const app = express();
 
- app.use("/",(req,res,next)=>{
-    // res.send("Handeling / route")   // This fn is middlewares
-     next();    
- });
-
- app.get("/user",
-    (req,res,next)=>{                  // This fn is middlewares
-    console.log("Handeling /user route");  
-    next()  
+app.use("/user",
+[(req,res,next)=>{
+    // Route Handeler1
+    console.log("Handeling the route user 1");
+    //  res.send("Response! 1");
+    next();
 },
-    (req,res,next)=>{              // This fn is middlewares
-    next()  
+(req,res,next)=>{
+    // Route Handeler2
+    console.log("Handeling the route user 2");
+    // res.send("Response! 2"); 
+    next();
+}],
+(req,res,next)=>{ 
+    console.log("Handeling the route user 3");
+    // res.send("Response! 3");   
+    next();
 },
-    (req,res,next)=>{              // This fn is request handeler
-    res.send("2nd route handeler")   
-},
-  
+(req,res,next)=>{ 
+    console.log("Handeling the route user 4");
+     res.send("Response! 4");
+    // next();   
+}
 );
-
-
 
 
 app.listen(7777,()=>{   // my sever is listening on 3000 port
     console.log("Server is successfully listening on port 7777....");
     
 });
-  
